dbf66b92cdc3cd6df7ef17584f65d03be1182ad0
Josko Latin <joskolatin@gmail.com>
2022-06-13 18:31:25 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,91 +1,55 @@
-// Define UI Vars
+//postavljanje varijabli
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-// Load all event listeners
+//loudanje event listenera
 loadEventListeners();
 
-// Load all event listeners
 function loadEventListeners() {
-  // DOM Load event
-  document.addEventListener('DOMContentLoaded', getTasks);
-  // Add task event
+  //dodavanje task eventa
   form.addEventListener('submit', addTask);
-  // Remove task event
+  //micanje taska sa x
   taskList.addEventListener('click', removeTask);
-  // Clear task event
+  //ciscenje taskova svih da btn-om
   clearBtn.addEventListener('click', clearTasks);
-  // Filter tasks event
+  //filter taskova event
   filter.addEventListener('keyup', filterTasks);
 }
 
-// Get Tasks from LS
-function getTasks() {
-  let tasks;
-  if (localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-
-  tasks.forEach(function (task) {
-    // Create li element
-    const li = document.createElement('li');
-    // Add class
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(task));
-    // Create new link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append the link to li
-    li.appendChild(link);
-
-    // Append li to ul
-    taskList.appendChild(li);
-  });
-}
-
-// Add Task
 function addTask(e) {
   if (taskInput.value === '') {
-    alert('Add a task');
+    alert('popuni formu');
   }
 
-  // Create li element
+  //kreiranje li elementa
   const li = document.createElement('li');
-  // Add class
+  //dodavanje klase li
   li.className = 'collection-item';
-  // Create text node and append to li
+  //kreiranje tekst nodea i stavljenje u li
   li.appendChild(document.createTextNode(taskInput.value));
-  // Create new link element
+  //kreiranje link elementa
   const link = document.createElement('a');
-  // Add class
   link.className = 'delete-item secondary-content';
-  // Add icon html
+  //dodavanje ikone x
   link.innerHTML = '<i class="fa fa-remove"></i>';
-  // Append the link to li
+  //apend link to li
   li.appendChild(link);
-
-  // Append li to ul
+  //dodavanje li u ul
   taskList.appendChild(li);
 
-  // Store in LS
-  storeTaskInLocalStorage(taskInput.value);
-
-  // Clear input
+  //ciscenje inputa nakon upisa taska
   taskInput.value = '';
+
+  //spremanje u lokalni torage
+  storeTaskInLocalStorage(taskInput.value);
 
   e.preventDefault();
 }
 
-// Store Task
+//funkcija za spremanje u lokalni storage
 function storeTaskInLocalStorage(task) {
   let tasks;
   if (localStorage.getItem('tasks') === null) {
@@ -99,57 +63,26 @@ function storeTaskInLocalStorage(task) {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Remove Task
+//micanje taskova
 function removeTask(e) {
   if (e.target.parentElement.classList.contains('delete-item')) {
-    if (confirm('Are You Sure?')) {
+    if (confirm('siguran')) {
       e.target.parentElement.parentElement.remove();
-
-      // Remove from LS
-      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
 }
 
-// Remove from LS
-function removeTaskFromLocalStorage(taskItem) {
-  let tasks;
-  if (localStorage.getItem('tasks') === null) {
-    tasks = [];
-  } else {
-    tasks = JSON.parse(localStorage.getItem('tasks'));
-  }
-
-  tasks.forEach(function (task, index) {
-    if (taskItem.textContent === task) {
-      tasks.splice(index, 1);
-    }
-  });
-
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-// Clear Tasks
+//funkcija za botun clear task
 function clearTasks() {
   // taskList.innerHTML = '';
 
-  // Faster
+  //brzi nacin
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
-
-  // https://jsperf.com/innerhtml-vs-removechild
-
-  // Clear from LS
-  clearTasksFromLocalStorage();
 }
 
-// Clear Tasks from LS
-function clearTasksFromLocalStorage() {
-  localStorage.clear();
-}
-
-// Filter Tasks
+//funkcija filtera
 function filterTasks(e) {
   const text = e.target.value.toLowerCase();
 
